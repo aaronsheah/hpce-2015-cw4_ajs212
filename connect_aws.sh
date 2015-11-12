dc9a164f4f1658c9ca6f40a451e1a90d16b7bb95
aaronsheah <aaronsheah@gmail.com>
2015-11-12 19:47:13 +0000

    v1.3.1 Added timing.sh to test speed of OpenCL version against original (CPU) version

--- connect_aws.sh
+++ connect_aws.sh
@@ -1,5 +1,21 @@
-# CW4
+############################# CW4 ###################################
+# Connect to AWS Instance
 ssh -i /Users/aaronsheah/Documents/Keys/ec2-keypair.pem admin@54.172.6.64
 
-# To update on AWS side
-# git pull --rebase
+# Upload and Download files through SSH
+## DOWNLOAD
+# scp -i /Users/aaronsheah/Documents/Keys/ec2-keypair.pem admin@54.172.6.64:/HPC/hpce-2015-cw4_ajs212/bin/dump.bmp dump.bmp
+# scp -i /Users/aaronsheah/Documents/Keys/ec2-keypair.pem admin@54.172.6.64:HPC/hpce-2015-cw4_ajs212/src/ajs212/step_world_v1_lambda.cpp step_world_v1_lambda.cpp
+## UPLOAD
+# scp -i /Users/aaronsheah/Documents/Keys/ec2-keypair.pem src/ajs212/step_world_v1_lambda.cpp admin@54.172.6.64:HPC/hpce-2015-cw4_ajs212/src/ajs212/step_world_v1_lambda.cpp
+# scp -i /Users/aaronsheah/Documents/Keys/ec2-keypair.pem src/ajs212/step_world_v2_function.cpp admin@54.172.6.64:HPC/hpce-2015-cw4_ajs212/src/ajs212/step_world_v2_function.cpp
+# scp -i /Users/aaronsheah/Documents/Keys/ec2-keypair.pem src/ajs212/step_world_v3_opencl.cpp admin@54.172.6.64:HPC/hpce-2015-cw4_ajs212/src/ajs212/step_world_v3_opencl.cpp
+# scp -i /Users/aaronsheah/Documents/Keys/ec2-keypair.pem src/ajs212/step_world_v3_kernel.cl admin@54.172.6.64:HPC/hpce-2015-cw4_ajs212/src/ajs212/step_world_v3_kernel.cl
+
+# Git cheat sheet
+# git tag -a v1.0 -m "v1.0 test"
+# git add *
+# git commit
+# git push
+# git pull
+#####################################################################
---
+++ timing.sh
@@ -0,0 +1,18 @@
+# Comparison between Original and OpenCL version
+time (bin/make_world | bin/step_world 0.1 1 > /dev/null)
+time (bin/make_world | bin/step_world_v3_opencl 0.1 1 > /dev/null)
+
+# Comparison between text and binary output
+time (bin/make_world 1000 0.1   0  > /dev/null)   # text format
+time (bin/make_world 1000 0.1   1  > /dev/null)   # binary format
+
+# Save output to a file to remove overhead of conversion between binary and text
+bin/make_world 1000 0.1 1 > /tmp/world.bin  # Save binary world to temp file
+
+# Measure the GPU overhead (no steps involved just setup)
+time (cat /tmp/world.bin | bin/step_world 0.1 0  1 > /dev/null) 
+time (cat /tmp/world.bin | bin/step_world_v3_opencl 0.1 0  1 > /dev/null)
+
+# Measure cost of one step
+time (cat /tmp/world.bin | bin/step_world 0.1 1  1 > /dev/null) 
+time (cat /tmp/world.bin | bin/$USER/step_world_v3_opencl 0.1 1  1 > /dev/null)
