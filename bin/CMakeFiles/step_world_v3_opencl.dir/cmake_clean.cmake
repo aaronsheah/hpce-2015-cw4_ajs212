be8867cc37a6a14d797b7ee9889fd3d1e09bfc1f
Debian <admin@ip-172-31-53-137.ec2.internal>
2015-11-12 18:02:44 +0000

    v1.2.1

---
+++ bin/CMakeFiles/step_world_v3_opencl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/step_world_v3_opencl.dir/src/heat.cpp.o"
+  "CMakeFiles/step_world_v3_opencl.dir/src/ajs212/step_world_v3_opencl.cpp.o"
+  "step_world_v3_opencl.pdb"
+  "step_world_v3_opencl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/step_world_v3_opencl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
