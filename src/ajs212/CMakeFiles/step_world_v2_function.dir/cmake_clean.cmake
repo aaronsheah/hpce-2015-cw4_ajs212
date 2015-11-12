e1a9507859eb9da10c074c39765ca1746b72d624
Debian <admin@ip-172-31-53-137.ec2.internal>
2015-11-12 15:11:05 +0000

    StepWorldV2Function

---
+++ src/ajs212/CMakeFiles/step_world_v2_function.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/step_world_v2_function.dir/src/heat.cpp.o"
+  "CMakeFiles/step_world_v2_function.dir/step_world_v2_function.cpp.o"
+  "step_world_v2_function.pdb"
+  "step_world_v2_function"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/step_world_v2_function.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
