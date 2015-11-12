3e96d879eba3eee5e981488e8b0b61bb317ce48c
Debian <admin@ip-172-31-53-137.ec2.internal>
2015-11-12 01:01:18 +0000

    CMake on Linux(AWS)

---
+++ bin/CMakeFiles/render_world.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/render_world.dir/src/heat.cpp.o"
+  "CMakeFiles/render_world.dir/src/render_world.cpp.o"
+  "render_world.pdb"
+  "render_world"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/render_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
