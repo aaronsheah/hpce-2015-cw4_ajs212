16e7ce1b09a1588a2cf48c0f21531cbd09370e21
Debian <admin@ip-172-31-53-137.ec2.internal>
2015-11-12 13:59:24 +0000

    step_world_v1_lambda

---
+++ src/ajs212/CMakeFiles/step_world_v1_lambda.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/step_world_v1_lambda.dir/src/heat.cpp.o"
+  "CMakeFiles/step_world_v1_lambda.dir/step_world_v1_lambda.cpp.o"
+  "step_world_v1_lambda.pdb"
+  "step_world_v1_lambda"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/step_world_v1_lambda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
