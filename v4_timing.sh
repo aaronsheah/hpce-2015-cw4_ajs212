3a1f22c7edf594dc0bdcdb677ea0f86ec27f983c
aaronsheah <aaronsheah@gmail.com>
2015-11-13 00:01:38 +0000

    v1.4.2. Fixed timing

--- v4_timing.sh
+++ v4_timing.sh
@@ -4,7 +4,7 @@ bin/make_world 1000 0.1 1 > /tmp/world.bin  # Save binary world to temp file
 
 echo '/************************************************************************/'
 echo 'Comparison between Original and OpenCL version'
-time (cat /tmp/world.bin | bin/step_world 0.1 1 > /dev/null)
+time (cat /tmp/world.bin | bin/step_world 0.1 100 1 > /dev/null)
 time (cat /tmp/world.bin | bin/step_world_v3_opencl 0.1 100 1 > /dev/null)
 time (cat /tmp/world.bin | bin/step_world_v4_double_buffered 0.1 100 1 > /dev/null)
 
